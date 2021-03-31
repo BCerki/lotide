191af86ed62cab11927931be8810ed7ae3b0f2ec
Brianna <briannac@shaw.ca>
2021-03-31 03:49:30 +0000

    added check for length

--- lotide/assertArraysEqual.js
+++ lotide/assertArraysEqual.js
@@ -1,5 +1,8 @@
 const eqArrays = function (arrayA, arrayB) {
   let checker = true;
+  if (arrayA.length !== arrayB.length) {
+    checker = false;
+  }
   for (let i = 0; i < arrayA.length; i++) {
     if (arrayA[i] !== arrayB[i]) {
       checker = false;
