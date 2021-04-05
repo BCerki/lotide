94d7a7403c5053e3b1e7cc6fd36d05f0b1346e00
Brianna <briannac@shaw.ca>
2021-04-05 23:35:06 +0000

    updated index.js to use object shorthand

--- lotide/index.js
+++ lotide/index.js
@@ -6,12 +6,12 @@ const countOnly = require('./countOnly');
 const eqArrays = require('./eqArrays');
 const eqObjects = require('./eqObjects');
 const findKey = require('./findKey');
-const findKeyByValue = require('.;/findKeyByValue');
+const findKeyByValue = require('./findKeyByValue');
 const flatten = require('./flatten');
 const head = require('./head');
-const letterPositions = require('.;/letterPositions');
+const letterPositions = require('./letterPositions');
 const map = require('./map');
-const middle = require('./ middle');
+const middle = require('./middle');
 const tail = require('./tail');;
 const takeUntil = require('./takeUntil');
 const without = require('./without');
@@ -19,21 +19,21 @@ const without = require('./without');
 
 
 module.exports = {
-  assertArraysEqual: assertArraysEqual,
-  assertEqual: assertEqual,
-  assertObjectEqual: assertObjectEqual,
-  countLetters: countLetters,
-  countOnly: countOnly,
-  eqArrays: eqArrays,
-  eqObjects: eqObjects,
-  findKey: findKey,
-  findKeyByValue: findKeyByValue,
-  flatten: flatten,
-  head: head,
-  letterPositions: letterPositions,
-  map: map,
-  middle: middle,
-  tail: tail,
-  takeUntil: takeUntil,
-  without: without
+  assertArraysEqual,
+  assertEqual,
+  assertObjectEqual,
+  countLetters,
+  countOnly,
+  eqArrays,
+  eqObjects,
+  findKey,
+  findKeyByValue,
+  flatten,
+  head,
+  letterPositions,
+  map,
+  middle,
+  tail,
+  takeUntil,
+  without
 }
